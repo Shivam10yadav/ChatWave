7bbbd58c96249d05bbb317c829679da69f7e34f4
Shivam Yadav <shivam10yadav05@gmail.com>
2025-12-01 12:55:17 +0530

    Fix API URL to use environment variable

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,8 @@
-import axios from'axios'
+import axios from 'axios'
 
-export const axiosInstance=axios.create({
-    baseURL:"http://localhost:5001/api",
-    withCredentials:true //send cookies with request
-    
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5001'
+
+export const axiosInstance = axios.create({
+    baseURL: `${API_URL}/api`,
+    withCredentials: true //send cookies with request
 })
