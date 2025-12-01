12bc11d4cab7c5c675ea6dfd22ea09f0f8a9f80f
Shivam Yadav <shivam10yadav05@gmail.com>
2025-12-01 13:54:14 +0530

    Fix cookie sameSite in both login and signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -90,12 +90,12 @@ export async function login(req, res) {
       expiresIn: "7d",
     });
 
-    res.cookie("jwt", token, {
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-      httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
-    });
+  res.cookie("jwt", token, {
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: process.env.NODE_ENV === "production",
+});
 
     res.status(200).json({ success: true, user });
   } catch (error) {
